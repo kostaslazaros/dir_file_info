b93735c6aab939036efa4fb8bbb7ffd8abc58a23
konstantinos lazaros <konlazaros@gmail.com>
2020-06-02 14:01:40 +0300

    chassis created

--- tree1.sh
+++ tree1.sh
@@ -1,10 +1,45 @@
 #!/bin/sh
 
-if [ -d "$@" ]; then #checks if the parameter is a directory
-    echo "Number of files is $(find "$@" -type f | wc -l)"
-    echo "Number of directories is $(find "$@" -type d | wc -l)"
+echo "Monitored directory:" $1
+echo "Depth:" $2
+echo " "
+if [ -d "$1" ]; then #checks if the parameter is a directory
+    echo "Number of directories: $(find "$1" -type d | wc -l)"
+    echo "Number of files: $(find "$1" -type f | wc -l)"
+    
     
 else
-    echo "[ERROR]  Please provide a directory." #if not a directory, print error message
-    exit 1
+    echo    
+    
 fi
+
+echo "Total size on disk:" 23
+echo " "
+
+
+echo "Permissions on files"
+echo " "
+
+
+echo "Large files"
+echo " "
+
+
+echo "Lastly created files"
+echo " "
+
+
+echo "Lastly modified files"
+echo " "
+
+
+echo "Dirs with more files"
+echo " "
+
+
+echo "Dirs with more files"
+echo " "
+
+
+echo "Largest dirs"
+echo " "
